54b1a768014eab83d2721c0b6c7b726a8d4273aa
abrilbabino <abril.nadia.babino984@gmail.com>
2026-05-03 17:14:53 -0300

    Dependencias de entidades

--- src/main/java/com/systeam/GestionDeProyectos/GestionDeProyectosApplication.java
+++ src/main/java/com/systeam/GestionDeProyectos/GestionDeProyectosApplication.java
@@ -2,8 +2,10 @@ package com.systeam.GestionDeProyectos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.systeam.shared.model")
 public class GestionDeProyectosApplication {
 
 	public static void main(String[] args) {
