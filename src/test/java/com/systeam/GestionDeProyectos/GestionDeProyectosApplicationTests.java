df5579ae3b7733b4a87624f59601ff3e960dce44
abrilbabino <abril.nadia.babino984@gmail.com>
2026-05-03 15:53:21 -0300

    Estructura inicial

---
+++ src/test/java/com/systeam/GestionDeProyectos/GestionDeProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systeam.GestionDeProyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
