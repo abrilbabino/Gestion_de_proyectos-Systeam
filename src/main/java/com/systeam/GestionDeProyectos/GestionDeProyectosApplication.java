dcef692ab836d392422bcc3b3f0b7778e72ded45
abrilbabino <abril.nadia.babino984@gmail.com>
2026-05-03 17:52:44 -0300

    Prueba lista

--- src/main/java/com/systeam/GestionDeProyectos/GestionDeProyectosApplication.java
+++ src/main/java/com/systeam/GestionDeProyectos/GestionDeProyectosApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
+import com.systeam.shared.model.Proyecto;
+
 @SpringBootApplication
-@EntityScan(basePackages = "com.systeam.backend.model")
+@EntityScan(basePackages = "com.systeam.shared.model")
 public class GestionDeProyectosApplication {
 
 	public static void main(String[] args) {
